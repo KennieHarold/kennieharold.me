a01127494b081cd6ef21d2082e7b20f429cde823
kennieHarold <kennie.harold@gmail.com>
2021-02-12 19:03:50 +0800

    Setup header layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './styles.css'
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import "./styles.css";
 
 const Header = () => {
+  const headerItems = [
+    "home",
+    "about",
+    "timeline",
+    "skills",
+    "portfolio",
+    "contact",
+  ];
+
   return (
     <header id="header">
-      <h1>Test</h1>
+      <Grid
+        id="header__grid"
+        container
+        justify="center"
+        alignItems="center"
+        direction="row"
+      >
+        {headerItems.map((item) => (
+          <Typography id={`header__grid__${item}`} variant="caption">
+            {item}
+          </Typography>
+        ))}
+      </Grid>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
