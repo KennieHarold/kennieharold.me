8e9b291674901615f1ace0402ccb08a54a4821fc
kennieHarold <kennie.harold@gmail.com>
2021-02-13 14:52:54 +0800

    Finalize home page

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Grid, Box, Typography } from "@material-ui/core";
+import coverImg from "../../assets/cover-img.png";
 import "./styles.css";
 
 const Home = () => {
   return (
     <section id="home">
-      <h1>Home</h1>
+      <Grid
+        id="home__grid"
+        container
+        justify="center"
+        alignItems="center"
+        direction="column"
+      >
+        <Box
+          id="home__grid__item"
+          className="home__grid__item"
+          display="flex"
+          flex={1}
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Typography id="home__grid__item__text1" variant="h5">
+            Hi, I'm
+          </Typography>
+          <Box
+            id="home__grid__item__text2"
+            display="flex"
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <hr />
+            <Typography variant="h2">Kennie Harold</Typography>
+            <hr />
+          </Box>
+          <Typography id="home__grid__item__text3" variant="h4">
+            I'm a software developer and I create beautiful apps
+          </Typography>
+          <Box
+            id="home__grid__item__img"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <img src={coverImg} alt="kennie harold" />
+          </Box>
+        </Box>
+      </Grid>
     </section>
-  )
-}
+  );
+};
 
 export default Home;
