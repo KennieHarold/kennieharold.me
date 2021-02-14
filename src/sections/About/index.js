482e9b87ebe7fb8ea77556f0af4ad879631ce2de
kennieHarold <kennie.harold@gmail.com>
2021-02-14 13:55:37 +0800

    Implement section title in about

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Grid, Box, Typography } from "@material-ui/core";
+import SectionTitle from "../../components/SectionTitle";
 import "./styles.css";
 
 const About = () => {
   return (
     <section id="about">
-      <h1>About</h1>
+      <Grid
+        id="about__grid"
+        container
+        justify="center"
+        alignItems="center"
+        direction="column"
+      >
+        <Box
+          id="about__grid__section-title"
+          display="flex"
+          flex={2.5}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <SectionTitle id="about__grid__section-title__about" title="about" />
+        </Box>
+        <Box
+          id="about__grid__content"
+          display="flex"
+          flex={7.5}
+          justifyContent="center"
+          alignItems="center"
+        >
+        </Box>
+      </Grid>
     </section>
   );
 };
