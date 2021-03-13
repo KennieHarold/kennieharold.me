78f45206618c276523fec907d3b4f068027e34d8
Kennie Harold Mantac <kennie.harold@gmail.com>
2021-03-13 10:29:48 +0800

    Init timeline

--- src/sections/Timeline/index.js
+++ src/sections/Timeline/index.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Grid, Box, Typography } from "@material-ui/core";
+import SectionTitle from "../../components/SectionTitle";
 import "./styles.css";
 
 const Timeline = () => {
   return (
     <section id="timeline">
-      <h1>Timeline</h1>
+      <Grid
+        id="timeline__grid"
+        container
+        justify="center"
+        alignItems="center"
+        direction="column"
+      >
+        <Box
+          id="timeline__grid__section-title"
+          display="flex"
+          flex={1}
+          justifyContent="center"
+          alignItems="flex-end"
+        >
+          <SectionTitle
+            id="timeline__grid__section-title__timeline"
+            title="timeline"
+          />
+        </Box>
+        <Box
+          id="timeline__grid__content"
+          display="flex"
+          flex={9}
+          justifyContent="center"
+          alignItems="center"
+        ></Box>
+      </Grid>
     </section>
   );
 };
