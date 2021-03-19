d0abe372d2fd1f23744241ffca4349714a8a5d35
Kennie Harold Mantac <kennie.harold@gmail.com>
2021-03-20 00:24:22 +0800

    Finish ui for contact

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -1,10 +1,122 @@
 import React from "react";
+import { Grid, Box, Typography, InputBase, Button } from "@material-ui/core";
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import { colorTheme } from "../../styles/themes";
+import SectionTitle from "../../components/SectionTitle";
 import "./styles.css";
 
+const useStyles = makeStyles({
+  contactQuestion: {
+    fontWeight: "bold",
+    marginBottom: 50,
+    textAlign: "center",
+  },
+  contactInputContainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  contactInputContent: {
+    background: "#E3EDF2",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+  },
+  contactInputBase: {
+    color: "gray",
+    width: "100%",
+  },
+});
+
 const Contact = () => {
+  const classes = useStyles();
+
   return (
     <section id="contact">
-      <h1>Contact</h1>
+      <ThemeProvider theme={colorTheme}>
+        <Grid
+          id="contact__grid"
+          container
+          justify="center"
+          alignItems="center"
+          direction="column"
+        >
+          <Box
+            id="contact__grid__section-title"
+            display="flex"
+            flex={1}
+            justifyContent="center"
+            alignItems="flex-end"
+          >
+            <SectionTitle
+              id="contact__grid__section-title__contact"
+              title="contact"
+            />
+          </Box>
+          <Box
+            id="contact__grid__content"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flex={9}
+            flexDirection="column"
+          >
+            <Typography
+              className={classes.contactQuestion}
+              variant="h5"
+              color="primary"
+            >
+              Have any question or want to discuss something?
+            </Typography>
+
+            <Box
+              id="contact__grid__content__form"
+              display="flex"
+              flexDirection="column"
+            >
+              <Box className={classes.contactInputContainer}>
+                <Box className={classes.contactInputContent}>
+                  <InputBase
+                    className={classes.contactInputBase}
+                    placeholder="Your name"
+                  />
+                </Box>
+              </Box>
+
+              <Box className={classes.contactInputContainer}>
+                <Box className={classes.contactInputContent}>
+                  <InputBase
+                    className={classes.contactInputBase}
+                    placeholder="Your email"
+                  />
+                </Box>
+              </Box>
+
+              <Box className={classes.contactInputContainer}>
+                <Box className={classes.contactInputContent}>
+                  <InputBase
+                    className={classes.contactInputBase}
+                    multiline
+                    rows={10}
+                    placeholder="Your message"
+                  />
+                </Box>
+              </Box>
+
+              <Button
+                variant="contained"
+                color="primary"
+                disableElevation
+                style={{ alignSelf: "flex-end" }}
+              >
+                Submit
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </ThemeProvider>
     </section>
   );
 };
